d0afb5cd05a1c5a1d7bf54911e4af57bc89e14d2
LENA EIVY <applena@gmail.com>
2019-05-21 19:00:30 -0700

    first object created

---
+++ class-07/demo/salmon-cookies-demo/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// need an array to hold the hours
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// object literal (multiple) for each store
+  // min customers per hour
+  // max customers per hour
+  // average cooker per customer
+  // an array of customer each hour based on the min and the max
+    // a random number between the min and the max
+  // an arry of cookies sold each hour
+    // multiply customers each hour by the average cookies sales per customer
+  // render this to the DOM
+  // total cookies for the day
+
+var pikePlace = {
+  minCustomersEachHour: 23,
+  maxCustomersEachHour: 65,
+  averageCookiesPerCustomer: 6.3,
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalCookiesForTheDay: 0,
+  render(){}
+}
+
+// helper function
+
+
+
+
+
+
+
+
